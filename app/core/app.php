82f7acab9bc1ed680b1df091d057a11b81bb00f7
cayvowcorir <cayvowcorir@gmail.com>
2016-03-22 14:27:26 +0300

    added Controllers

--- app/core/app.php
+++ app/core/app.php
@@ -6,12 +6,18 @@ class App
   protected $params= [];
   public function __construct()
 {
-  $this->parseUrl();
+  $url=$this->parseUrl();
+  if(file_exists('../app/controllers/'.$url[0].'.php'))
+  {
+    $this->controller=$url[0];
+    unset($url[0]);
+  }
+  require_once '../app/controllers/'.$this->controller.'.php';
 }
 
 }
 public function parseUrl(){
    if(isset($_GET['url'])){
-    echo $_GET['url'];
+    return $url = explode('/', filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
    }
 }
